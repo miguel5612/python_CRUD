4c07655bf53ab06059113e76bc71a62cf153313a
miguel5612 <miguelangelcu@ufps.edu.co>
2020-06-16 21:59:00 -0500

    Fixed key without '

--- app/middlewares/tools.py
+++ app/middlewares/tools.py
@@ -21,7 +21,7 @@ def keyValueFromArregloWithAnd(arreglo):
     concatenado = ''
     contador = 0
     for key,value in arreglo.items():
-        concatenado += "'" + key + "'" + "='" + value + "'"
+        concatenado += key + "='" + value + "'"
         contador+=1
         if(contador<len(arreglo)): concatenado += ' AND '
     return concatenado
