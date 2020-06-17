90c31b9a2ef1cd0a7cc1ece2f965d3865a4eb278
miguel5612 <miguelangelcu@ufps.edu.co>
2020-06-16 20:04:07 -0500

    Removed data

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
    
    
 class Config():
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://liker:6574*Liker@localhost:3306/aiscrap'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:pass@localhost:3306/dbName'
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CELERY_BROKER_URL = 'amqp://localhost//'
